7ec8e54a63f737d4e16ed8d9b7bb7a3fa21f98af
ushiken <u5h1k3n@gmail.com>
2019-11-12 11:46:39 +0900

    implemented nextChar()

--- src/getSource.c
+++ src/getSource.c
@@ -88,3 +88,76 @@ void closeSource()
     fclose(fpi);
     fclose(fptex);
 }
+
+void errorNoCheck()
+{
+    if (errorNo++ > MAXERROR) {
+        fprintf(fptex, "too many errors\n\\end{document}\n");
+        printf("abort compilation\n");
+        exit(1);
+    }
+}
+
+void errorType(char *m)
+{
+    // TODO
+}
+
+void errorInsert(KeyId k)
+{
+    // TODO
+}
+
+void errorMissingId()
+{
+    // TODO
+}
+
+void errorMissingOp()
+{
+    // TODO
+}
+
+void errorDelete()
+{
+    // TODO
+}
+
+void errorMessage(char *m)
+{
+    fprintf(fptex, "$^{%s}$", m);
+    errorNoCheck();
+}
+
+void errorF(char *m)
+{
+    errorMessage(m);
+    fprintf(fptex, "fatal errors\n\\end{document}\n");
+    if (errorNo)
+        printf("total %d errors\n", errorNo);
+    printf("abort compilation\n");
+    exit(1);
+}
+
+int errorN()
+{
+    return errorNo;
+}
+
+char nextChar()
+{
+    char ch;
+    if (lineIndex == -1) {
+        if (fgets(line, MAXLINE, fpi) != NULL) {
+            puts(line);
+            lineIndex = 0;
+        } else {
+            errorF("end of file\n"); // TODO
+        }
+    }
+    if ((ch = line[lineIndex++]) == '\n') {
+        lineIndex = -1;
+        return '\n';
+    }
+    return ch;
+}
